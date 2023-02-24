69479b3d0a99c483226e083802105bdd83bc6a3f
Dawnnote <lshoon7454@gmail.com>
2023-02-24 20:35:09 +0900

    required_edit

--- golf_review/golf/urls.py
+++ golf_review/golf/urls.py
@@ -13,14 +13,20 @@ urlpatterns = [
     # profile urls
     path("users/<int:user_id>/", views.ProfileView.as_view(), name="profile"),
     path("user/<int:user_id>/reviews/", views.UserReviewListView.as_view(), name="user-review-list"),
-
     path("set-profile/", views.ProfileSetView.as_view(), name="profile-set"),
     path("edit-profile/", views.ProfileUpdateView.as_view(), name="profile-update"),
 
+    # comment url
+    path(
+        'reviews/<int:review_id>/comments/create/',
+        views.CommentCreateView.as_view(),
+        name='comment-create',
+    ),
 
+
+    # other page url
     path("screen", views.GotoScreenGolf.as_view(), name="go-to-screengolf"),
     path("practice", views.GotoPracticeGolf.as_view(), name="go-to-practicegolf"),
-
 
     path('post', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail')
--- golf_review/golf/views.py
+++ golf_review/golf/views.py
@@ -6,12 +6,12 @@ from django.views.generic import (
     CreateView, UpdateView,
     DeleteView,
     )
-from braces.views import LoginRequiredMixin, UserPassesTestMixin
-from allauth.account.models import EmailAddress
+from braces.views import LoginRequiredMixin
 from allauth.account.views import PasswordChangeView
-from .models import Review, User, Post, Comment
+
+from .mixins import LoginAndVerificationRequiredMixin, LoginAndOwnershipRequiredMixin
+from .models import Review, User, Post, Comment, UserComment
 from .forms import ReviewForm, ProfileForm, CommentForm, UserCommentForm
-from .functions import confirmation_required_redirect
 
 
 # Create your views here.
@@ -33,13 +33,10 @@ class ReviewDetailView(DetailView):
         return context
 
 
-class ReviewCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+class ReviewCreateView(LoginAndVerificationRequiredMixin, CreateView):
     model = Review
     form_class = ReviewForm
     template_name = "golf/review_form.html"
-
-    redirect_unauthenticated_users = True
-    raise_exception = confirmation_required_redirect
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -48,39 +45,40 @@ class ReviewCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     def get_success_url(self):
         return reverse("review-detail", kwargs={"review_id":self.object.id})
 
-    def test_func(self, user):
-        return EmailAddress.objects.filter(user=user, verified=True).exists()
 
-
-class ReviewUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class ReviewUpdateView(LoginAndOwnershipRequiredMixin, UpdateView):
     model = Review
     form_class = ReviewForm
     template_name = "golf/review_form.html"
     pk_url_kwarg = "review_id"
 
-    raise_exception = True
-
     def get_success_url(self):
         return reverse("review-detail", kwargs={"review_id":self.object.id})
 
-    def test_func(self, user):
-        review = self.get_object()
-        return review.author == user
 
-
-class ReviewDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class ReviewDeleteView(LoginAndOwnershipRequiredMixin, DeleteView):
     model = Review
     template_name = "golf/review_confirm_delete.html"
     pk_url_kwarg = "review_id"
 
-    raise_exception = True
-
     def get_success_url(self):
         return reverse("index")
 
-    def test_func(self, user):
-        review = self.get_object()
-        return review.author == user
+
+class CommentCreateView(LoginAndVerificationRequiredMixin, CreateView):
+    http_method_names = ['post']
+
+    model = UserComment
+    form_class = UserCommentForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.review = Review.objects.get(id=self.kwargs.get('review_id'))
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('review-detail', kwargs={'review_id': self.kwargs.get('review_id')})
+
 
 
 class ProfileView(DetailView):
@@ -140,6 +138,8 @@ class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
         return reverse('profile', kwargs={"user_id": self.request.user.id})
 
 
+
+
 class GotoScreenGolf(ListView):
     model = Review
     template_name = "map/golf_screengolf.html"
@@ -147,8 +147,6 @@ class GotoScreenGolf(ListView):
 class GotoPracticeGolf(ListView):
     model = Review
     template_name = "map/golf_practicegolf.html"
-
-
 
 
 class PostListView(ListView):
