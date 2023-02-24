97203cb879604ee6a3c8daa1013d47489d407656
Dawnnote <lshoon7454@gmail.com>
2023-02-24 18:47:52 +0900

    update

--- golf_review/golf/models.py
+++ golf_review/golf/models.py
@@ -48,6 +48,18 @@ class Review(models.Model):
     def __str__(self):
         return self.title
 
+class UserComment(models.Model):
+    content = models.TextField(max_length=500, blank=False)
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content[:30]
+
+
 
 
 class Post(models.Model):
