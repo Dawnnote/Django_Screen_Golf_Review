0648186bec790eea1c4a193075ae4d53a59605f5
Dawnnote <lshoon7454@gmail.com>
2023-02-24 19:28:13 +0900

    Update admin

--- golf_review/golf/admin.py
+++ golf_review/golf/admin.py
@@ -1,11 +1,44 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Review, Post
+from django.contrib.contenttypes.admin import GenericStackedInline
+
+from .models import User, Review, Post, UserComment, Like
 
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
-UserAdmin.fieldsets += (("Custom fields", {"fields":("nickname", "profile_pic", "intro")}),)
+class CommentInline(admin.StackedInline):
+    model = UserComment
 
-admin.site.register(Review)
+class LikeInline(GenericStackedInline):
+    model = Like
+
+class UserInline(admin.StackedInline):
+    model = User.following.through
+    fk_name = 'to_user'
+    verbose_name = 'Followers'
+    verbose_name_plural = 'Followers'
+
+UserAdmin.fieldsets += (("Custom fields", {"fields":("nickname", "profile_pic", "intro", "following")}),)
+UserAdmin.inlines = (UserInline,)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    inlines = (
+        CommentInline,
+        LikeInline,
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (
+        LikeInline,
+    )
+
+
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(Post)
+
+admin.site.register(UserComment, CommentAdmin)
+admin.site.register(Like)
